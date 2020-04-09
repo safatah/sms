eb98822792e97317efb03ba2ac676feeb7927cd1
Savannah Tomas <stkytomas@gmail.com>
2020-04-09 15:55:24 -0500

    models for business info, signup and user info done. data binding next'

--- MvcMovie/Models/SignUp.cs
+++ MvcMovie/Models/SignUp.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using MvcMovie.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HappyHelper.Models
 {
-    public class SignUp
-    {
-        public int Id { get; set; }
+    public class SignUp : UserInfo
+    { 
         public string Username { get; set; }
         public string Password { get; set; }
     }
